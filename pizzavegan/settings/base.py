490ebd58de48e69e41152ccaad91e634a5ce4ff0
Cory Coward <cory@c2softwaresolutions.com>
2020-11-11 20:54:30 -0600

    Change base_url in settings

--- pizzavegan/settings/base.py
+++ pizzavegan/settings/base.py
@@ -183,7 +183,7 @@ WAGTAIL_USER_CUSTOM_FIELDS = []
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = 'http://example.com'
+BASE_URL = 'https://pizzavegan.com'
 
 # Users and Authentication
 AUTH_USER_MODEL = 'users.CustomUser'
