09e6357a3e1c1625ccb45cfd334fe52251e31f4f
Cory Coward <cory@c2softwaresolutions.com>
2020-12-15 19:13:35 -0600

    Add route to Products to filter by category

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django import forms
+from django.http import Http404
+from django.shortcuts import render
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
@@ -7,10 +9,11 @@ from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 
 
-class ProductListPage(Page):
+class ProductListPage(RoutablePageMixin, Page):
     intro = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
@@ -24,6 +27,20 @@ class ProductListPage(Page):
         regular_products = [pg for pg in product_pages if pg.specific.is_featured is False]
         context['product_pages'] = featured_products + regular_products
         return context
+
+    @route(r'^category/(?P<category_slug>[-\w]*)/$', name='product_category_view')
+    def category_view(self, request, category_slug):
+        context = self.get_context(request)
+        try:
+            category = ProductCategory.objects.get(slug=category_slug)
+        except Exception:
+            category = None
+
+        if category is None:
+            raise Http404('The selected category does not exist.')
+
+        context['product_pages'] = ProductSinglePage.objects.live().public().filter(categories__in=[category]).order_by('-first_published_at')
+        return render(request, 'products/product_list_page.html', context)
 
 
 class ProductSinglePage(Page):
