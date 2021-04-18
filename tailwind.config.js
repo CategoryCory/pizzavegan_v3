3b0f7c912561b1f2faafbf2c92bd6bd5fe8cbca5
Cory Coward <cory@c2softwaresolutions.com>
2021-04-17 20:05:58 -0500

    Add TailwindCSS to project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+      "{articles,home,pages,pizzavegan,pizzerias,products,recipes,search,templates}/**/*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
