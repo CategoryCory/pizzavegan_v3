9762e85499aade86e12fbb86f5fceefb29c70468
Cory Coward <cory@c2softwaresolutions.com>
2021-04-29 15:46:35 -0500

    Add and set up Map component

--- pizzeria_map_svjs/src/App.svelte
+++ pizzeria_map_svjs/src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import Map from './components/Map.svelte';
+	export let ready;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svelte:head>
+	<script defer async
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBIIbGAPSDhXreX1zK_WoOXZSGR1ktxw6I&callback=initMap">
+	</script>
+	<script defer
+		src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+	</script>
+</svelte:head>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<div class="w-full h-screen lg:grid lg:grid-cols-2">
+	<div class="h-1/3 lg:h-full">
+		{#if ready}
+			<Map />
+		{:else}
+			<div class="h-full grid place-content-center">
+				<h2 class="text-2xl text-gray-400">Loading map...</h2>
+			</div>
+		{/if}
+	</div>
+	<div class="">
+		<div>This is the search area.</div>
+		<div>This is the results area.</div>
+	</div>
+</div>
--- pizzeria_map_svjs/src/main.js
+++ pizzeria_map_svjs/src/main.js
@@ -1,10 +1,14 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
+	target: document.getElementById("pizzeria_app"),
 	props: {
-		name: 'world'
+		ready: false
 	}
 });
+
+window.initMap = function ready() {
+	app.$set({ ready: true });
+}
 
 export default app;
