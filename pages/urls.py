3abe52565951818a0af685d52f06ff097ebb78ac
Cory Coward <cory@c2softwaresolutions.com>
2021-05-10 15:06:48 -0500

    Remove signup pages temporarily

--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     # path('meet-the-editor/', views.MeetTheEditorView.as_view(), name='meet_the_editor'),
     path('contact/', views.ContactUsView.as_view(), name='contact_us'),
-    path('pizzavegan-signup/', views.pizzavegan_signup_view, name='pizzavegan_signup'),
-    path('register/', views.RegisterPizzeriaView.as_view(), name='register_pizzeria'),
+    # path('pizzavegan-signup/', views.pizzavegan_signup_view, name='pizzavegan_signup'),
+    # path('register/', views.RegisterPizzeriaView.as_view(), name='register_pizzeria'),
 ]
