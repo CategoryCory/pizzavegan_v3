52ac147417d87e8b4d7a30bd7bb063558bea5531
Cory Coward <cory@c2softwaresolutions.com>
2021-05-19 10:28:26 -0500

    Remove unused styles from map app

--- pizzeria_map_svjs/src/App.svelte
+++ pizzeria_map_svjs/src/App.svelte
@@ -1,98 +1,83 @@
 <script>
-	import Map from './components/Map.svelte';
-	import SearchBar from './components/SearchBar.svelte';
-	import SearchResults from './components/SearchResults.svelte';
-	import { isLoading } from "./stores";
+  import Map from "./components/Map.svelte";
+  import SearchBar from "./components/SearchBar.svelte";
+  import SearchResults from "./components/SearchResults.svelte";
 
-	export let ready;
+  export let ready;
 </script>
 
 <svelte:head>
-	<script defer async
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtCwMX4StPEEWhjUJ-yfWyPwSwe-YD0kU&callback=initMap">
-	</script>
-	<script defer
-		src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
-	</script>
+  <script
+    defer
+    async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtCwMX4StPEEWhjUJ-yfWyPwSwe-YD0kU&callback=initMap">
+  </script>
+  <script
+    defer
+    src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
+  </script>
 </svelte:head>
 
 <main class="map-container">
-	<div class="map">
-		{#if ready}
-			<Map />
-		{:else}
-			<div class="map__loading-container">
-				<h2 class="map__loading-message">Loading map...</h2>
-			</div>
-		{/if}
-	</div>
-	<div class="search-container">
-		<SearchBar />
-		<SearchResults />
-	</div>
+  <div class="map">
+    {#if ready}
+      <Map />
+    {:else}
+      <div class="map__loading-container">
+        <h2 class="map__loading-message">Loading map...</h2>
+      </div>
+    {/if}
+  </div>
+  <div class="search-container">
+    <SearchBar />
+    <SearchResults />
+  </div>
 </main>
 
 <style>
-	.map-container {
-		width: 100%;
-	}
+  .map-container {
+    width: 100%;
+  }
 
-	.map {
-		height: 250px;
-	}
+  .map {
+    height: 250px;
+  }
 
-	.map__loading-container {
-		height: 100%;
-		display: grid;
-		place-items: center;
-	}
+  .map__loading-container {
+    height: 100%;
+    display: grid;
+    place-items: center;
+  }
 
-	.map__loading-message {
-		font-size: 2rem;
-		color: rgb(168, 162, 158);
-	}
+  .map__loading-message {
+    font-size: 2rem;
+    color: rgb(168, 162, 158);
+  }
 
-	.search-container {
-		height: inherit;
-		padding-top: 0.75em;
-	}
+  .search-container {
+    height: inherit;
+    padding-top: 0.75em;
+  }
 
-	.loading-results {
-        display: grid;
-        place-items: center;
+  @media screen and (min-width: 640px) {
+    .map {
+      height: 350px;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .map-container {
+      height: 750px;
+      display: grid;
+      grid-template-columns: repeat(2, minmax(0, 1fr));
     }
 
-    .loading-results p {
-        padding: 2rem 0;
-        font-family: "Jost", "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-        font-size: 1.75rem;
-        font-weight: 800;
-        color: #D6D3D1;
+    .map {
+      height: 100%;
     }
 
-	@media screen and (min-width: 640px) {
-		.map {
-			height: 350px;
-		}
-	}
-
-	@media screen and (min-width: 1024px) {
-		.map-container {
-			height: 750px;
-			display: grid;
-			grid-template-columns: repeat(2, minmax(0, 1fr));
-		}
-
-		.map {
-			height: 100%;
-		}
-
-		.search-container {
-			max-height: 100%;
-		}
-
-		.loading-results p {
-            font-size: 3rem;
-        }
-	}
+    .search-container {
+      max-height: 100%;
+    }
+  }
 </style>
