65f4967d36276bf2e2ddb8afd65a6c3075905b4f
Cory Coward <cory@c2softwaresolutions.com>
2021-05-25 17:20:48 -0500

    Show user in django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Account Information', {'fields': ('username', 'email', 'first_name', 'last_name', )}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', )}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined', )}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
