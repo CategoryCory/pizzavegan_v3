c59d90259a32e680475e83cc4a320f6c36e30197
Cory Coward <cory@c2softwaresolutions.com>
2021-05-25 17:24:02 -0500

    Change list display and filter for users

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,10 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_active', ]
+    list_editable = ['is_active', ]
+    list_filter = ['is_active', ]
+    list_per_page = 20
     fieldsets = (
         ('Account Information', {'fields': ('username', 'email', 'first_name', 'last_name', )}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', )}),
