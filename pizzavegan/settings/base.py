f2e3bf0be9ea248f057bef529e860b39e6182cfb
Cory Coward <cory@c2softwaresolutions.com>
2021-05-26 21:22:07 -0500

    Add styles for login and signup forms

--- pizzavegan/settings/base.py
+++ pizzavegan/settings/base.py
@@ -209,6 +209,17 @@ ACCOUNT_UNIQUE_EMAIL = True
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 
+ACCOUNT_FORMS = {
+    'login': 'users.forms.CustomLoginForm',
+    'signup': 'users.forms.CustomSignupForm',
+    'add_email': 'allauth.account.forms.AddEmailForm',
+    'change_password': 'allauth.account.forms.ChangePasswordForm',
+    'set_password': 'allauth.account.forms.SetPasswordForm',
+    'reset_password': 'allauth.account.forms.ResetPasswordForm',
+    'reset_password_from_key': 'allauth.account.forms.ResetPasswordKeyForm',
+    'disconnect': 'allauth.socialaccount.forms.DisconnectForm',
+}
+
 # Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 from wagtail.users.forms import UserEditForm, UserCreationForm
 
+from allauth.account.forms import PasswordField, LoginForm, SignupForm
+
 
 class CustomUserEditForm(UserEditForm):
     pass
@@ -10,3 +12,27 @@ class CustomUserEditForm(UserEditForm):
 
 class CustomUserCreationForm(UserCreationForm):
     pass
+
+
+class CustomLoginForm(LoginForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        self.fields['login'] = forms.EmailField(label='Email Address')
+        self.fields['login'].widget.attrs['placeholder'] = 'Email'
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full rounded border-gray-400 focus:border-brand focus:ring-0 transition my-1'
+            })
+
+
+class CustomSignupForm(SignupForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomSignupForm, self).__init__(*args, **kwargs)
+        self.fields['email'] = forms.EmailField(label='Email Address')
+        self.fields['password2'] = PasswordField(label='Confirm password')
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'w-full rounded border-gray-400 focus:border-brand focus:ring-0 transition my-1'
+            })
