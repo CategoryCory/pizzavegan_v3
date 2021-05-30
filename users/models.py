0acb9f5a0e8011e06746801644df4ed81e54b8d8
Cory Coward <cory@c2softwaresolutions.com>
2021-05-30 10:13:40 -0500

    Add company name to users model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    company_name = models.CharField(max_length=100, verbose_name='Company/Pizzeria Name')
+
+    def __str__(self) -> str:
+        return self.email
