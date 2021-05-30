f7e9a2d27dfc16294752df48e06d59d373b4f442
Cory Coward <cory@c2softwaresolutions.com>
2021-05-30 10:22:35 -0500

    Remove company name from users model

--- users/models.py
+++ users/models.py
@@ -3,7 +3,4 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    company_name = models.CharField(max_length=100, verbose_name='Company/Pizzeria Name')
-
-    def __str__(self) -> str:
-        return self.email
+    pass
