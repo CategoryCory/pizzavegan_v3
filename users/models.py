eebbe7afa1c5d1d319d14fba1ecb13148b2dd889
Cory Coward <cory@c2softwaresolutions.com>
2021-06-01 09:30:33 -0500

    Add company name to customuser model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    company_name = models.CharField(max_length=100, verbose_name='Company/Pizzeria Name')
+
+    def __str__(self) -> str:
+        return self.email
