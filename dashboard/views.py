a21ab9cfceb9a9389b69306f28f3c278c7306550
Cory Coward <cory@c2softwaresolutions.com>
2021-06-05 10:40:52 -0500

    Add loginrequired to dashboard home

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
 
@@ -8,29 +9,7 @@ from profiles.models import CustomUser, PizzeriaProfile
 CustomUser = get_user_model()
 
 
-@login_required
-def dashboard_home_view(request):
-    profile = PizzeriaProfile.objects.get(user_account=request.user)
-
-    context = {
-        'user_name': request.user.username,
-        'first_name': request.user.first_name,
-        'last_name': request.user.last_name,
-        'email': request.user.email,
-        'company_name': request.user.company_name,
-        'profile_description': profile.description,
-        'profile_facebook': profile.facebook,
-        'profile_twitter': profile.twitter,
-        'profile_instagram': profile.instagram,
-        'profile_tiktok': profile.tiktok,
-        'profile_youtube': profile.youtube,
-        'profile_online_ordering': profile.online_ordering,
-        'profile_logo': profile.pizzeria_logo
-    }
-    return render(request, 'dashboard/dashboard-home.html', context)
-
-
-class DashboardHomeView(DetailView):
+class DashboardHomeView(LoginRequiredMixin, DetailView):
     model = CustomUser
     template_name = 'dashboard/dashboard-home.html'
 
