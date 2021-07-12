7cb67e2dfd54cd2c4a2c617d47ac8c3a19ece9e1
Cory Coward <cory@c2softwaresolutions.com>
2021-07-12 11:19:47 -0500

    Clean up pizzerias app and remove unneeded files

--- pizzerias/urls.py
+++ pizzerias/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import PizzeriaDetail, pizzeria_listings
+from .views import pizzeria_listings
 
 app_name = 'pizzerias'
 urlpatterns = [
     path('', pizzeria_listings, name='pizzeria_list'),
-    path('<int:pk>/', PizzeriaDetail.as_view(), name='pizzeria_detail'),
 ]
