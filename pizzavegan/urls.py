1a7088fb002ded4bd2b549a9d858927917c77cf0
Cory Coward <cory@c2softwaresolutions.com>
2021-09-27 12:31:08 -0500

    Add sitemap

--- pizzavegan/urls.py
+++ pizzavegan/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
@@ -19,6 +20,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('documents/', include(wagtaildocs_urls)),
     path('search/', search_views.search, name='search'),
+    path('sitemap.xml', sitemap),
 ]
 
 
